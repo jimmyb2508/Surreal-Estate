3a33a62fbc87b3ef7e5aab1c0aee716b860893af
jimmyb2508 <james.barrington1985@gmail.com>
2019-12-11 18:06:32 +0000

    Added fields to AddProperty.jsx

--- src/components/AddProperty.jsx
+++ src/components/AddProperty.jsx
@@ -8,6 +8,12 @@ class AddProperty extends React.Component {
     this.state = {
       fields: {
         title: '',
+        type: '',
+        bedrooms: '',
+        bathrooms: '',
+        price:'',
+        city: '',
+        email:''
       }
     }
   }
@@ -18,14 +24,93 @@ class AddProperty extends React.Component {
     console.log(this.state.fields);
   }
 
+  handleFieldChange = (event) => {
+    this.setState({
+      fields: {...this.state.fields, [event.target.name]: event.target.value},
+    });
+  };
+
   render() {
     return (
-  <div className="AddProperty" onSubmit={this.handleAddProperty}>
-    <form>
+  <div className="AddProperty">
+    <form onSubmit={this.handleAddProperty}>
       <label>
-        Add: 
-        <button type="submit" />
+      Title:
+        <input 
+        name="title" 
+        value={this.state.fields.title}
+        onChange={this.handleFieldChange} 
+        />
       </label>
+      <br></br>
+      <label>
+        Type:
+        <select
+          name="type"
+          value={this.state.fields.type}
+          onChange={this.handleFieldChange}>
+            <option value="flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End Of Terrace">End Of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+        </select>
+      </label>
+      <br></br>
+      <label>
+        Bedrooms:
+        <input
+          name="bedrooms"
+          value={this.state.fields.bedrooms}
+          onChange={this.handleFieldChange}
+          />
+      </label>
+      <br></br>
+      <label>
+        Bathrooms:
+        <input
+          name="bathrooms"
+          value={this.state.fields.bathrooms}
+          onChange={this.handleFieldChange}
+          />
+      </label>
+      <br></br>
+      <label>
+        Price:
+        <input
+        name="price"
+        value={this.state.fields.price}
+        onChange={this.handleFieldChange}
+        />
+      </label>
+      <br></br>
+      <label>
+        City:
+        <select
+        name="city"
+        value={this.state.fields.city}
+        onChange={this.handleFieldChange}>
+        <option defaultValue="Manchester">Manchester</option>
+        <option value="Leeds">Leeds</option>
+        <option value="Sheffield">Sheffield</option>
+        <option value="Liverpool">Liverpool</option>
+        </select>
+      </label>
+      <br></br>
+        <label>
+          email:
+      <input
+      name="email"
+      type="email"
+      value={this.state.fields.email}
+      onChange={this.handleFieldChange}
+      />
+      </label>
+        <button type="submit">
+          <label>Add</label>
+        </button>
     </form>
   </div>
     );
