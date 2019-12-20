32fcda4c0992b0b5e379f0be4a78943ce1ae93ea
jimmyb2508 <james.barrington1985@gmail.com>
2019-12-20 23:08:42 +0000

    Friday Work

--- src/components/AddProperty.jsx
+++ src/components/AddProperty.jsx
@@ -19,8 +19,8 @@ class AddProperty extends React.Component {
   }
 
   handleAddProperty = (event) => {
-    this.setState({})
     event.preventDefault();
+    // this.setState({})
     console.log(this.state.fields);
   }
 
@@ -34,83 +34,91 @@ class AddProperty extends React.Component {
     return (
   <div className="AddProperty">
     <form onSubmit={this.handleAddProperty}>
-      <label>
-      Title:
-        <input 
-        name="title" 
-        value={this.state.fields.title}
-        onChange={this.handleFieldChange} 
-        />
-      </label>
-      <br></br>
-      <label>
-        Type:
-        <select
-          name="type"
-          value={this.state.fields.type}
-          onChange={this.handleFieldChange}>
-            <option value="flat">Flat</option>
-            <option value="Detached">Detached</option>
-            <option value="Semi-Detached">Semi-Detached</option>
-            <option value="Terraced">Terraced</option>
-            <option value="End Of Terrace">End Of Terrace</option>
-            <option value="Cottage">Cottage</option>
-            <option value="Bungalow">Bungalow</option>
-        </select>
-      </label>
-      <br></br>
-      <label>
-        Bedrooms:
-        <input
-          name="bedrooms"
-          value={this.state.fields.bedrooms}
-          onChange={this.handleFieldChange}
+      <div className="propertyTitle">
+        <label>
+          Title:
+          <input 
+            name="title" 
+            value={this.state.fields.title}
+            onChange={this.handleFieldChange} 
           />
-      </label>
-      <br></br>
-      <label>
-        Bathrooms:
-        <input
-          name="bathrooms"
-          value={this.state.fields.bathrooms}
-          onChange={this.handleFieldChange}
+        </label>
+      </div>
+      <div className="propertyType">
+        <label>
+          Type:
+          <select
+            name="type"
+            value={this.state.fields.type}
+            onChange={this.handleFieldChange}>
+              <option value="flat">Flat</option>
+              <option value="Detached">Detached</option>
+              <option value="Semi-Detached">Semi-Detached</option>
+              <option value="Terraced">Terraced</option>
+              <option value="End Of Terrace">End Of Terrace</option>
+              <option value="Cottage">Cottage</option>
+              <option value="Bungalow">Bungalow</option>
+          </select>
+        </label>
+      </div>
+      <div className="propertyBedrooms">
+        <label>
+          Bedrooms:
+          <input
+            name="bedrooms"
+            value={this.state.fields.bedrooms}
+            onChange={this.handleFieldChange}
           />
-      </label>
-      <br></br>
-      <label>
-        Price:
-        <input
-        name="price"
-        value={this.state.fields.price}
-        onChange={this.handleFieldChange}
-        />
-      </label>
-      <br></br>
-      <label>
-        City:
-        <select
-        name="city"
-        value={this.state.fields.city}
-        onChange={this.handleFieldChange}>
-        <option defaultValue="Manchester">Manchester</option>
-        <option value="Leeds">Leeds</option>
-        <option value="Sheffield">Sheffield</option>
-        <option value="Liverpool">Liverpool</option>
-        </select>
-      </label>
-      <br></br>
+        </label>
+      </div>
+      <div className="propertyBathrooms">
+        <label>
+          Bathrooms:
+          <input
+            name="bathrooms"
+            value={this.state.fields.bathrooms}
+            onChange={this.handleFieldChange}
+            />
+        </label>
+      </div>
+      <div className="propertyPrice">
+        <label>
+          Price:
+          <input
+            name="price"
+            value={this.state.fields.price}
+            onChange={this.handleFieldChange}
+           />
+        </label>
+      </div>
+      <div className="propertyCity">
+        <label>
+          City:
+          <select
+            name="city"
+            value={this.state.fields.city}
+            onChange={this.handleFieldChange}>
+              <option defaultValue="Manchester">Manchester</option>
+              <option value="Leeds">Leeds</option>
+              <option value="Sheffield">Sheffield</option>
+              <option value="Liverpool">Liverpool</option>
+          </select>
+        </label>
+      </div>
+      <div className="propertyEmail">
         <label>
           email:
-      <input
-      name="email"
-      type="email"
-      value={this.state.fields.email}
-      onChange={this.handleFieldChange}
-      />
-      </label>
-        <button type="submit">
-          <label>Add</label>
-        </button>
+          <input
+            name="email"
+            type="email"
+            value={this.state.fields.email}
+            onChange={this.handleFieldChange}
+          />
+        </label>
+      </div>
+          <button type="submit">
+            <label>Add</label>
+          </button>
     </form>
   </div>
     );
