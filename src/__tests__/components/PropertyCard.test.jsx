918a0c935b05cebc030828ff33e3eb0ef79bf063
jimmyb2508 <james.barrington1985@gmail.com>
2019-12-25 12:35:57 +0000

    Finished testing PropertyCard component

--- src/__tests__/components/PropertyCard.test.jsx
+++ src/__tests__/components/PropertyCard.test.jsx
@@ -2,6 +2,111 @@ import PropertyCard from '../../components/PropertyCard.jsx';
 import { shallow } from 'enzyme';
 import React from 'react';
 
-it('renders OK', () => {
+it('renders', () => {
   shallow(<PropertyCard />);
+});
+
+it('renders a title', () => {
+  const wrapper = shallow(<PropertyCard 
+    title="JamesHouse"
+    type="Terraced"
+    bedrooms="2"
+    bathrooms="2"
+    price="185000"
+    city="Manchester"
+    email="james.barrington1985@gmail.com"
+    />,
+  )
+  const text = wrapper.find('.PropertyCardTitle').text();
+  expect(text).toEqual('JamesHouse');
+});
+
+it('renders a type', () => {
+  const wrapper = shallow(<PropertyCard 
+  title="JamesHouse"
+  type="Terraced"
+  bedrooms="2"
+  bathrooms="2"
+  price="185000"
+  city="Manchester"
+  email="james.barrington1985@gmail.com"
+    />,
+  )
+  const text = wrapper.find('.PropertyCardType').text();
+  expect(text).toEqual('Terraced');
+});
+
+it('renders a bedroom', () => {
+  const wrapper = shallow(<PropertyCard 
+  title="JamesHouse"
+  type="Terraced"
+  bedrooms="2"
+  bathrooms="2"
+  price="185000"
+  city="Manchester"
+  email="james.barrington1985@gmail.com"
+    />,
+  )
+  const text = wrapper.find('.PropertyCardBedrooms').text();
+  expect(text).toEqual('2');
+});
+
+it('renders a bathroom', () => {
+  const wrapper = shallow(<PropertyCard 
+    title="JamesHouse"
+    type="Terraced"
+    bedrooms="2"
+    bathrooms="2"
+    price="185000"
+    city="Manchester"
+    email="james.barrington1985@gmail.com"
+    />,
+  )
+  const text = wrapper.find('.PropertyCardBathrooms').text();
+  expect(text).toEqual('2');
+});
+
+it('renders a price', () => {
+  const wrapper = shallow(<PropertyCard
+    title="JamesHouse"
+    type="Terraced"
+    bedrooms="2"
+    bathrooms="2"
+    price="185000"
+    city="Manchester"
+    email="james.barrington1985@gmail.com"
+   />,
+  )
+  const text = wrapper.find('.PropertyCardPrice').text();
+  expect(text).toEqual('185000');
+})
+
+it('renders a city', () => {
+  const wrapper = shallow(<PropertyCard
+    title="JamesHouse"
+    type="Terraced"
+    bedroooms="2"
+    bathrooms="2"
+    price="185000"
+    city="Manchester"
+    email="james.barrington1985@gmail.com"
+    />,
+   )
+   const text = wrapper.find('.PropertyCardCity').text();
+   expect(text).toEqual('Manchester')
+});
+
+it('renders an email', () => {
+  const wrapper = shallow(<PropertyCard 
+    title="JamesHouse"
+    type="Terraced"
+    bedrooms="2"
+    bathrooms="2"
+    price="185000"
+    city="Manchester"
+    email="james.barrington1985@gmail.com"
+    />,
+  )
+  const text = wrapper.find('.PropertyCardEmail').text();
+  expect(text).toEqual('james.barrington1985@gmail.com')
 });
--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -2,7 +2,31 @@ import React, { Component } from 'react';
 
 class PropertyCard extends React.Component {
   render() {
-
+    return (
+      <div>
+        <div className="PropertyCardTitle">
+          <span>{this.props.title}</span>
+        </div>
+        <div className="PropertyCardType">
+          <span>{this.props.type}</span>
+        </div>
+        <div className="PropertyCardBedrooms">
+          <span>{this.props.bedrooms}</span>
+        </div>
+        <div className="PropertyCardBathrooms">
+          <span>{this.props.bathrooms}</span>
+        </div>
+        <div className="PropertyCardPrice">
+          <span>{this.props.price}</span>
+        </div>
+        <div className="PropertyCardCity">
+          <span>{this.props.city}</span>
+        </div>
+        <div className="PropertyCardEmail">
+          <span>{this.props.email}</span>
+        </div>
+      </div>
+    );
   }
 }
 
