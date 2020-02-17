23ffd5a52f7566e9c2605c715081c93d4a93494c
jimmyb2508 <james.barrington1985@gmail.com>
2020-02-17 20:54:21 +0000

    Styled Property Card Component (Hard Coded for styling)

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
+import '../styles/PropertyCard.css';
+import { FaBed } from 'react-icons/fa';
+import { FaBath } from 'react-icons/fa';
+import { FaPoundSign } from 'react-icons/fa';
+import { GoMail } from 'react-icons/go';
 
 const PropertyCard = props => {
-  const {
-    title,
-    type,
-    city,
-    bedrooms,
-    bathrooms,
-    price,
-    email,
-  } = props;
+  const { title, type, city, bedrooms, bathrooms, price, email } = props;
 
-    return (
-      <div className="propertyCard">
-        <span className="title">{title}</span>
-        <span className="type">{type}</span>
-        <span className="bedrooms">{bedrooms}</span>
-        <span className="bathrooms">{bathrooms}</span>
-        <span className="price">{price}</span>
-        <span className="city">{city}</span>
-        <span className="email">{email}</span>
-      </div>
-    );
-}
+  //temp - hardcoded to allow for styling
+
+  return (
+    <div className="card">
+      <span className="title">18th Century Rural Home</span>
+      <br />
+      <br />
+      <span className="type">Terraced House</span>
+      <br />
+      <br />
+      <span className="city">Manchester</span>
+      <br />
+      <br />
+      <FaBed className="bed" />
+      <span className="bedrooms">2</span>
+      <br />
+      <br />
+      <FaBath className="bath" />
+      <span className="bathrooms">2</span>
+      <br />
+      <br />
+      <FaPoundSign className="pound" />
+      <span className="price">180,000</span>
+      <br />
+      <br />
+      <GoMail className="mail" />
+      <span className="email">jamesb@gmail.com</span>
+    </div>
+  );
+};
 
 export default PropertyCard;
